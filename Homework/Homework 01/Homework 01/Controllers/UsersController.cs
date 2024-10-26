ef53e260612beaa4d38ef80dd78490acd90d8fe4
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-10-26 22:35:28 +0200

    added homework 01

---
+++ Homework/Homework 01/Homework 01/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Homework_01.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+       
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetUsers()
+        {
+            return Ok(StaticDb.Users);
+        }
+
+      
+        [HttpGet("{name}")]
+        public ActionResult<string> GetUser(string name)
+        {
+            var user = StaticDb.Users.FirstOrDefault(u => u.Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ Homework/Homework 01/Homework 01/Models/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework_01.Models
+{
+    public static class StaticDb
+    {
+        // Static list of user names
+        public static List<string> Users { get; } = new List<string>
+        {
+            "Alice",
+            "Bob",
+            "Charlie",
+            "Diana"
+        };
+    }
+}
